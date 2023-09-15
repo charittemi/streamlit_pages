510c00ceb832877fbf2db34f757e49ac80b48e72
charittemi <risamyst@gmail.com>
2023-09-15 16:48:34 +0900

    modified:   pages/page_3.py

--- pages/page_3.py
+++ pages/page_3.py
@@ -9,8 +9,8 @@ st.text('')
 # Create a Markdown-It instance
 md = markdown_it.MarkdownIt()
 
-# Markdown text to be converted
-markdown_text = """
+# Markdown text input
+markdown_text = st.text_area('Enter Markdown Text', """
 # My Markdown Example
 
 This is a paragraph of text in *italic* and **bold**.
@@ -19,13 +19,15 @@ This is a paragraph of text in *italic* and **bold**.
 - Item 2
 
 [Link to Google](https://www.google.com)
-"""
+""")
 
 # Convert Markdown to HTML
 html_output = md.render(markdown_text)
 
-# Print the HTML result
-print(html_output)
+# Display HTML output
+st.write('HTML Output:')
+st.write(html_output, unsafe_allow_html=True)
+
 
 
 
