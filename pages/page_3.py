99511cec8bc22941f04a8a1845d372e95305f850
charittemi <risamyst@gmail.com>
2023-09-15 16:42:43 +0900

    modified:   pages/page_3.py

--- pages/page_3.py
+++ pages/page_3.py
@@ -1,9 +1,15 @@
 ﻿import streamlit as st
+import markdown_it
 
 st.title('Page 3')
 st.caption('テストページ作成中です')
 st.subheader('')
 st.text('')
+
+# Create a Markdown-It instance
+md = markdown_it.MarkdownIt()
+
+# Markdown text to be converted
 markdown_text = """
 # My Markdown Example
 
@@ -14,6 +20,12 @@ This is a paragraph of text in *italic* and **bold**.
 
 [Link to Google](https://www.google.com)
 """
-print(markdown_text)
-st.text('ok')
+
+# Convert Markdown to HTML
+html_output = md.render(markdown_text)
+
+# Print the HTML result
+print(html_output)
+
+
 
