e4f419dcc2541388a398755703601f7ffa0c075a
charittemi <risamyst@gmail.com>
2023-09-15 16:36:42 +0900

    modified:   pages/page_3.py

--- pages/page_3.py
+++ pages/page_3.py
@@ -4,6 +4,15 @@ st.title('Page 3')
 st.caption('テストページ作成中です')
 st.subheader('')
 st.text('')
+markdown_text = """
+# My Markdown Example
 
+This is a paragraph of text in *italic* and **bold**.
+
+- Item 1
+- Item 2
+
+[Link to Google](https://www.google.com)
+"""
 st.text('ok')
 
