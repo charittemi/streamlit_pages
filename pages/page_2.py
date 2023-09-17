a6da929a7326b5585eb299409079ea5a36f89a92
charittemi <risamyst@gmail.com>
2023-09-17 22:03:00 +0900

    c

--- pages/page_2.py
+++ pages/page_2.py
@@ -11,20 +11,13 @@ st.image(image, width=250)
 st.text('ok')
 
 # Markdown
-markdown = """
+markdown = r"""
 
 $e^{i\theta}$ は $\cos\theta$ と $i\sin\theta$ の和です。
 
 $$
 e^{i\theta} = \cos\theta + i\sin\theta
 $$
-
-$e^{i\\th}$ は $cos\\th$ と $i\\sin\\th$ の和です。
-
-$$
-e^{i\\th} = cos\\th + i\\sin\\th
-$$
-
 
 """
 st.markdown(markdown)
