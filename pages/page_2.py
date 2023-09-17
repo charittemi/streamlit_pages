ca95acc3edb7a93787a7de913063ce48a5b4b6d5
charittemi <risamyst@gmail.com>
2023-09-17 22:46:01 +0900

    c

--- pages/page_2.py
+++ pages/page_2.py
@@ -10,30 +10,17 @@ image=Image.open('./data/okojo.jpg')
 st.image(image, width=250)
 st.text('')
 
-# Markdown
+# Markdownセクション
 markdown = r"""
-
-$$
-e^{i\theta} = \cos\theta + i\sin\theta
-$$
-
-$e^{i\theta}$ は $\cos\theta$ と $i\sin\theta$ の和です。
 
 """
 st.markdown(markdown)
 
+
+
+st.write('')
+#コードセクション
 code = '''
-
-<!-- Markdownで数式を記述する際、文字列の先頭に` r `をつけることで、pythonの
-raw stringとして扱われ、エスケープシーケンスが無効化されます。
-これにより、Markdown内で\記号をそのまま使用することが可能になります。-->
-
-markdown = r"""
-
-$e^{i\theta}$ は $\cos\theta$ と $i\sin\theta$ の和です。
-
-"""
-st.markdown(markdown)
 
 '''
 st.code(code)
