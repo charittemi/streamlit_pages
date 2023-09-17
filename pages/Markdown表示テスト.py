047780aa7409914d927c4e5293003bf65fe5ec2f
charittemi <risamyst@gmail.com>
2023-09-17 21:21:48 +0900

    c

--- pages/Markdown表示テスト.py
+++ pages/Markdown表示テスト.py
@@ -2,32 +2,32 @@
 import markdown_it
 
 st.title('Page 3')
-st.caption('Markdownテスト用')
-st.subheader('')
+st.caption('Markdown表示テスト用')
 st.text('')
 
 # Create a Markdown-It instance
 md = markdown_it.MarkdownIt()
 
+
 # Markdown text input
-markdown_text = st.text_area('Markdown編集欄（Ctrl + Enterで表示反映）', """
----
+st.subheader('Markdownテキスト編集欄')
+markdown_text = st.text_area('（Ctrl + Enterで表示反映）', """
 # Title
 ## Subtitle
 ### Another deeper title
 ---
 This is a paragraph of text in *italic* and **bold**.
->- Item 1
->- Item 2
+- Item 1
+- Item 2
 
 [Link to Google](https://www.google.com)
-""", height=250)
+""", height=300)
 
 # Convert Markdown to HTML
 html_output = md.render(markdown_text)
-
+st.text('')
 # Display HTML output
-st.write('Markdown ＞＞ HTML の出力結果:')
+st.subheader('Markdown ＞＞ HTML の出力結果:')
 st.write(html_output, unsafe_allow_html=True)
 
 
