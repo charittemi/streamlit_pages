e77a9e79d768048ff5bdc8d611ef3e007622f5af
charittemi <risamyst@gmail.com>
2023-09-27 13:10:03 +0900

    c

--- pages/Marp_slides.py
+++ pages/Marp_slides.py
@@ -3,11 +3,12 @@ import streamlit as st
 
 st.title('Marp Slide')
 st.subheader('')
-st.text('')
+st.text('作成中')
+url = "https://charittemi-slide1.streamlit.app/Slide_1"
 
-# Slide1.htmlを埋め込む
-with open('Slide1.html', 'r', encoding='utf-8') as html_file:
-    slide_html = html_file.read()
+# # Slide1.htmlを埋め込む
+# with open('Slide1.html', 'r', encoding='utf-8') as html_file:
+#     slide_html = html_file.read()
 
-st.components.v1.html(slide_html, width=720, height=540)
-st.text('')
+# st.components.v1.html(slide_html, width=720, height=540)
+# st.text('')
