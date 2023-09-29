4b8b8678d7eb0331ab9d320578d8d6e736826286
charittemi <risamyst@gmail.com>
2023-09-29 17:40:56 +0900

    m

--- pages/graph_1.py
+++ pages/graph_1.py
@@ -31,20 +31,3 @@ plt.legend()
 st.pyplot(plt)
 
 
-col1,col2=st.columns([2,3]) # カラムの比率2:3で設定
-with col1:
-    df= pd.read_csv('./data/testdata.csv',index_col='No')
-    
-    # matplot棒グラフを生成する
-    fig, ax = plt.subplots()  
-    ax.bar(df.index, df['Value'])
-
-    # タイトル、X軸ラベル、Y軸ラベルを設定する
-    ax.set_title('matplotlib graph')
-    ax.set_xlabel('No')
-    ax.set_ylabel('Value')
-
-    # Streamlitにプロットを表示する
-    st.pyplot(fig)
-with col2:
-    st.text('グラフの説明')
