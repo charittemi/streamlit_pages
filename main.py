19e34b4dc66bf47fe7af43b3188d302c9002ad34
charittemi <risamyst@gmail.com>
2023-09-30 12:53:11 +0900

    m

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from PIL import Image
 #image=Image.open('./data/okojo_side.jpg')
 @st.cache
 def load_image(path):
-    return Image.open('./data/okojo_side.jpg')
+    return Image.open(path)
 
 def main():
     st.title('はじめに')
