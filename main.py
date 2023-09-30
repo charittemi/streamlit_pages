a6bae9ff4009ea40ff798229c776723bfb309b67
charittemi <risamyst@gmail.com>
2023-09-30 12:56:15 +0900

    m

--- main.py
+++ main.py
@@ -3,22 +3,14 @@
 import streamlit as st
 from PIL import Image
 
-# 初期化時に一度だけ実行される処理
-#image=Image.open('./data/okojo_side.jpg')
-@st.cache
-def load_image(path):
-    return Image.open(path)
+st.title('はじめに')
+st.caption('本サイトはpython学習記録として作成したものであり、各種ライブラリを試しています.')
+st.subheader('About')
+st.text('streamlitでテストページを作ってみました')
 
-def main():
-    st.title('はじめに')
-    st.caption('本サイトはpython学習記録として作成したものであり、各種ライブラリを試しています.')
-    st.subheader('About')
-    st.text('streamlitでテストページを作ってみました')
-
-    # 予めキャッシュしておいた画像を使用
-    image = load_image('./data/okojo_side.jpg')
-    st.image(image, width=250)
-    st.text('')
+image=Image.open('./data/okojo_side.jpg')
+st.image(image, width=250)
+st.text('')
     
 #コード表示用
 code='''
